4885f5634625659a2be7b8b62a7be33b2158a8e2
Tommi Virtanen <tv@eagain.net>
2015-07-28 21:53:22 -0700

    Smaller deltas at the ends of ranges
    Without this, the dimmest settings are still too bright.

--- delta.go
+++ delta.go
@@ -14,10 +14,19 @@ func delta(cur, min, max, step int32) int32 {
 
 // delta returns a value approximately 5% of the range in the given
 // direction, without crossing min or max or under/overflowing.
+//
+// When closer than two steps from the end of the range, the increment
+// is 0.5%.
 func delta5(cur, min, max int32, increase bool) int32 {
 	step := (max - min) / 20
 	if step < 1 {
 		step = 1
+	}
+	if max-cur <= 2*step || cur-min <= 2*step {
+		step = (max - min) / 200
+		if step < 1 {
+			step = 1
+		}
 	}
 	if !increase {
 		step = -step
